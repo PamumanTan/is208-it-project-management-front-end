6c73d218bbc43f764790aa6713c4cc3d98ec5f45
unknown <truongthienloc2@gmail.com>
2024-05-10 22:52:40 +0700

    feature(manage-account): create manage-account page

---
+++ src/pages/Admin/ManageAccount/ManageAccount.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import Paper from '@mui/material/Paper'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import Button from '@mui/material/Button'
+
+export default function ManageAccount() {
+    return (
+        <div className="container p-8 flex flex-col gap-4">
+            <h1 className="text-2xl font-bold">Quản Lí Tài Khoản</h1>
+            <div className="flex justify-end">
+                <Button variant='contained' >Tạo tài khoản</Button>
+            </div>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table" className='[&_th]:font-bold'>
+                    <TableHead>
+                        <TableRow className='[&_th]:text-lg'>
+                            <TableCell align='center'>Mã giáo viên</TableCell>
+                            <TableCell align='center'>Email</TableCell>
+                            <TableCell align='center'>Họ và tên</TableCell>
+                            <TableCell align='center'>Action</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody className='[&_td]:text-center [&_th]:text-center'>
+                        <TableRow
+                            // key={row.name}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell component="th" scope="row">
+                                GV001
+                            </TableCell>
+                            <TableCell>giaovien001@gmail.com</TableCell>
+                            <TableCell>Nguyen Van A</TableCell>
+                            <TableCell>
+                                <div className="flex flex-row gap-2 justify-center">
+                                    <Button variant='contained' color='info'>Sửa</Button>
+                                    <Button variant='contained' color='error'>Xóa</Button>
+                                </div>
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
+    )
+}
--- src/routes/rootRoute.jsx
+++ src/routes/rootRoute.jsx
@@ -1,4 +1,6 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
+import { AdminLayout } from '~/components/AdminLayout'
+import { ManageAccount } from '~/pages/Admin/ManageAccount'
 
 const rootRoute = createBrowserRouter(
     createRoutesFromElements(
@@ -8,6 +10,11 @@ const rootRoute = createBrowserRouter(
                 {/* HomePage */}
                 <Route path="/" element={<h1>Hello</h1>} /> 
                 <Route path="/class" element={<></>} />
+            </Route>
+
+            {/* Admin layout */}
+            <Route path='/admin' element={<AdminLayout />}>
+                <Route path='manage-account' element={<ManageAccount />} />
             </Route>
         </Route>
     ),
